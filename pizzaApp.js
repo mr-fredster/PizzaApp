96d9ca8f1278332d2bc4e159e32bb31159bdb48d
Freddy Rastede <freddyrastede@gmail.com>
2018-01-07 20:30:23 -0500

    V01R01
    Optimized for remainder

--- pizzaApp.js
+++ pizzaApp.js
@@ -6,7 +6,7 @@ function processForm(){
 	const largeSize = pizzaArea(16);
 		
 	var numberOfPeople = Number(document.mainForm.numberOfPeople.value);
-	var portion = (mediumSize/4); //each person receives 2 average slices
+	var portion = (largeSize/4); //each person receives 2 average slices
 	var appetite = numberOfPeople * portion; 
 	var small = false;
 	var medium = false;
@@ -15,48 +15,96 @@ function processForm(){
 	var numMedium = 0;
 	var numLarge = 0;
 	var remaining = 0;
-		
+	var optimize = false;
+	
+	var tempAppetite = 0;
+	
 	if(document.mainForm.small.checked){small = true};
 	if(document.mainForm.medium.checked){medium = true};
 	if(document.mainForm.large.checked){large = true};
-		
+			
 	if(large == true){
-		for(;(appetite/largeSize)>=1;){
-			numLarge++;
-			appetite -= largeSize;
-		}
-	}
-	
-	if(medium == true){
-		for(;(appetite/mediumSize)>=1;){
-			numMedium++;
-			appetite -= mediumSize;
-		}
-	}
-	
-	if(small == true){
-		for(;(appetite/smallSize)>=1;){
-			numSmall++;
-			appetite -= smallSize;
-		}
-	}
-	
-	if(appetite != 0){
-		if((appetite - smallSize <= 0) && (small == true)){
-			numSmall++;
-			remaining = smallSize - appetite;
-		}
-		else if((appetite - mediumSize <= 0) && (medium == true)){
-			numMedium++;
-			remaining = mediumSize - appetite;
-		}
-		else{
-			if(large == true){
-				numLarge++;
-				remaining = largeSize - appetite;
+		numLarge = Math.floor(appetite/largeSize);
+		console.log(numLarge);
+		appetite = appetite - (numLarge*largeSize);
+		if(appetite > 0) {
+			if((medium && small) == true){
+				tempAppetite = appetite - largeSize;
+			
+				if((appetite - smallSize) <= 0 && (appetite - smallSize > tempAppetite)){
+					numSmall++;
+					appetite -= smallSize;
+				}
+				else if((appetite - mediumSize) <= 0 && (appetite - mediumSize > tempAppetite)){
+					numMedium++;
+					appetite -= mediumSize;
+				}
+				else if((appetite - (mediumSize + smallSize)) <= 0 && (appetite - (mediumSize + smallSize) > tempAppetite)){
+					numSmall++;
+					numMedium++;
+					appetite -= mediumSize + smallSize;
+				}else{
+					numLarge++;
+					appetite = tempAppetite;
+				}
+			}
+			else if(medium == true){
+				tempAppetite = appetite - largeSize;
+			
+				if((appetite - mediumSize) <= 0 && (appetite - mediumSize > tempAppetite)){
+					numMedium++;
+					appetite -= mediumSize;
+				}
+				else{
+					numLarge++;
+					appetite = tempAppetite;
+				}
+			}
+			else if(small == true){
+				tempAppetite = appetite - largeSize;
+
+				if((appetite - smallSize) <= 0 && (appetite - smallSize > tempAppetite)){
+					numSmall++;
+					appetite -= smallSize;
+				}
+				else{
+					numLarge++;
+					appetite = tempAppetite;
+				}
 			}
 		}
 	}
+	else if(medium == true){
+		numMedium = Math.floor(appetite/mediumSize);
+		appetite = appetite - (numMedium*mediumSize);
+		if(appetite > 0){
+			if(small == true){
+				tempAppetite = appetite - mediumSize;
+				
+				if((appetite - smallSize) <= 0 && (appetite - smallSize > tempAppetite)){
+					numSmall++;
+					appetite -= smallSize;
+				}
+				else{
+					numMedium++;
+					appetite = tempAppetite;
+				}
+			}
+		}
+	}
+	else if(small == true){
+		numSmall = Math.floor(appetite/smallSize);
+		appetite = appetite - (numSmall*smallSize);
+		if(appetite > 0){
+			numSmall++;
+			appetite -= smallSize;
+		}
+	}	
+	else{
+		//do nothing
+	}
+			
+	remaining = -appetite;
 	
 	document.mainForm.numSmall.value = numSmall;
 	document.mainForm.numMedium.value = numMedium;
@@ -64,6 +112,7 @@ function processForm(){
 	document.mainForm.remainder.value = remaining.toFixed(2);
 	document.mainForm.slices.value = (remaining/(portion/2)).toFixed(2) + " slices";
 }
+
 
 function pizzaArea(diameter){
 	radius = diameter / 2;
