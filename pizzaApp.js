1140d09b0fa18be1ad2924dd4125ec19109800db
Freddy Rastede <freddyrastede@gmail.com>
2018-01-29 23:55:11 -0500

    V01R04
    - Added Hunger selection for individual users.
    -Results now display amount of slices for only available sizes.
    - Added cost calculation.
    - Added Optimize for Cost radio button option (grayed out)

--- pizzaApp.js
+++ pizzaApp.js
@@ -1,125 +1,69 @@
 function processForm(){
-	
-	//pizza size areas
-	const smallSize = pizzaArea(10);
-	const mediumSize = pizzaArea(12);
-	const largeSize = pizzaArea(16);
 		
-	var numberOfPeople = Number(document.mainForm.numberOfPeople.value);
-	var portion = (largeSize/4); //each person receives 2 average slices
-	var appetite = numberOfPeople * portion; 
-	var small = false;
-	var medium = false;
-	var large = false;
-	var numSmall = 0;
-	var numMedium = 0;
-	var numLarge = 0;
-	var remaining = 0;
-	var optimize = false;
-	
-	var tempAppetite = 0;
-	
-	if(document.mainForm.small.checked){small = true};
-	if(document.mainForm.medium.checked){medium = true};
-	if(document.mainForm.large.checked){large = true};
-			
-	if(large == true){
-		numLarge = Math.floor(appetite/largeSize);
-		console.log(numLarge);
-		appetite = appetite - (numLarge*largeSize);
-		if(appetite > 0) {
-			if((medium && small) == true){
-				tempAppetite = appetite - largeSize;
-			
-				if((appetite - smallSize) <= 0 && (appetite - smallSize > tempAppetite)){
-					numSmall++;
-					appetite -= smallSize;
-				}
-				else if((appetite - mediumSize) <= 0 && (appetite - mediumSize > tempAppetite)){
-					numMedium++;
-					appetite -= mediumSize;
-				}
-				else if((appetite - (mediumSize + smallSize)) <= 0 && (appetite - (mediumSize + smallSize) > tempAppetite)){
-					numSmall++;
-					numMedium++;
-					appetite -= mediumSize + smallSize;
-				}else{
-					numLarge++;
-					appetite = tempAppetite;
-				}
-			}
-			else if(medium == true){
-				tempAppetite = appetite - largeSize;
-			
-				if((appetite - mediumSize) <= 0 && (appetite - mediumSize > tempAppetite)){
-					numMedium++;
-					appetite -= mediumSize;
-				}
-				else{
-					numLarge++;
-					appetite = tempAppetite;
-				}
-			}
-			else if(small == true){
-				tempAppetite = appetite - largeSize;
-
-				if((appetite - smallSize) <= 0 && (appetite - smallSize > tempAppetite)){
-					numSmall++;
-					appetite -= smallSize;
-				}
-				else{
-					numLarge++;
-					appetite = tempAppetite;
-				}
-			}
-			else{
-				numLarge++;
-				appetite -= largeSize;
-			}
-		}
+	var smallPizza = {
+		id: "small",
+		isAvailable: document.mainForm.small.checked,
+		size: pizzaArea(10),
+		quantity: 0,
+		cost: document.mainForm.smallPrice.value,
+		subtotalCost: function(){return this.cost * this.quantity;}
 	}
-	else if(medium == true){
-		numMedium = Math.floor(appetite/mediumSize);
-		appetite = appetite - (numMedium*mediumSize);
-		if(appetite > 0){
-			if(small == true){
-				tempAppetite = appetite - mediumSize;
-				
-				if((appetite - smallSize) <= 0 && (appetite - smallSize > tempAppetite)){
-					numSmall++;
-					appetite -= smallSize;
-				}
-				else{
-					numMedium++;
-					appetite = tempAppetite;
-				}
-			}
-			else{
-				numMedium++;
-				appetite -= mediumSize;
-			}
-		}
-	}
-	else if(small == true){
-		numSmall = Math.floor(appetite/smallSize);
-		appetite = appetite - (numSmall*smallSize);
-		if(appetite > 0){
-			numSmall++;
-			appetite -= smallSize;
-		}
-	}	
-	else{
-		//do nothing
-	}
-			
-	remaining = -appetite;
 	
-	document.getElementById("numSmall").innerHTML = "Number of Small Pizzas: " + numSmall;
-	document.getElementById("numMedium").innerHTML = "Number of Medium Pizzas: " + numMedium;
-	document.getElementById("numLarge").innerHTML = "Number of Large Pizzas: " + numLarge;
-	document.getElementById("remainder").innerHTML = "Number of Remaining Sq. Inches: " + remaining.toFixed(2);
-	document.getElementById("slices").innerHTML = "Number of Remaining Slices: " + (remaining/(portion/2)).toFixed(2) + " slices";
+	var mediumPizza = {
+		id: "medium",
+		isAvailable: document.mainForm.medium.checked,
+		size: pizzaArea(12),
+		quantity: 0,
+		cost: document.mainForm.mediumPrice.value,
+		subtotalCost: function(){return this.cost * this.quantity;}
+	}
 	
+	var largePizza = {
+		id: "large",
+		isAvailable: document.mainForm.large.checked,
+		size: pizzaArea(16),
+		quantity: 0,
+		cost: document.mainForm.largePrice.value,
+		subtotalCost: function(){return this.cost * this.quantity;}
+	}
+	
+	var patrons = {
+		id: "patrons",
+		liteEaters: (parseInt(document.mainForm.numberOfLiteEaters.value) * 1 * (largePizza.size/8)),
+	    regularEaters: (parseInt(document.mainForm.numberOfRegularEaters.value) * 2 * (largePizza.size/8)),
+		heavyEaters: (parseInt(document.mainForm.numberOfHeavyEaters.value) * 3 * (largePizza.size/8)),
+		appetite: function(){return this.liteEaters + this.regularEaters + this.heavyEaters;},
+		leftovers: 0
+	}
+	
+	minimizeLeftovers(smallPizza, mediumPizza, largePizza, patrons);
+	//minimizeCost(smallPizza, mediumPizza, largePizza, patrons);
+	
+	totalCost = (smallPizza.subtotalCost() + mediumPizza.subtotalCost() + largePizza.subtotalCost()).toFixed(2);
+	remainingSmallSlices = (patrons.leftovers/(smallPizza.size/4)).toFixed(2);
+	remainingMediumSlices = (patrons.leftovers/(mediumPizza.size/8)).toFixed(2);
+	remainingLargeSlices = (patrons.leftovers/(largePizza.size/8)).toFixed(2);
+	
+	var remainingSlices = "Number of Remaining Slices: ";
+	
+	if (largePizza.quantity > 0 && largePizza.isAvailable){
+		remainingSlices = remainingSlices + remainingLargeSlices + " large slices";
+	} else if (mediumPizza.quantity > 0 && mediumPizza.isAvailable){
+		remainingSlices = remainingSlices + remainingMediumSlices + " medium slices";
+	} else if (smallPizza.quantity > 0 && smallPizza.isAvailable){
+		remainingSlices = remainingSlices + remainingSmallSlices + " small slices";
+	}else{
+		remainingSlices = "Error: select some available pizza options!";
+	}
+		
+	document.getElementById("numSmall").innerHTML = "Number of Small Pizzas: " + smallPizza.quantity;
+	document.getElementById("numMedium").innerHTML = "Number of Medium Pizzas: " + mediumPizza.quantity;
+	document.getElementById("numLarge").innerHTML = "Number of Large Pizzas: " + largePizza.quantity;
+	document.getElementById("remainder").innerHTML = "Number of Remaining Sq. Inches: " + patrons.leftovers.toFixed(2);
+	
+	document.getElementById("slices").innerHTML = remainingSlices;
+		
+	document.getElementById("totalCost").innerHTML = "Total Cost: $" + totalCost;
 }
 
 
@@ -128,3 +72,173 @@ function pizzaArea(diameter){
 	return Math.PI * radius * radius;
 }
 
+function minimizeLeftovers(smallPizza, mediumPizza, largePizza, patrons){
+	var tempAppetite = 0;
+	var appetite = 0;
+	
+	if(largePizza.isAvailable == true){
+		
+		largePizza.quantity = Math.floor(patrons.appetite()/largePizza.size);
+		appetite = patrons.appetite() - (largePizza.quantity*largePizza.size);
+				
+		if(appetite > 0) {
+			if((mediumPizza.isAvailable && smallPizza.isAvailable) == true){
+				tempAppetite = appetite - largePizza.size;
+			
+				if((appetite - smallPizza.size) <= 0 && (appetite - smallPizza.size > tempAppetite)){
+					smallPizza.quantity++;
+					appetite -= smallPizza.size;
+				}
+				else if((appetite - mediumPizza.size) <= 0 && (appetite - mediumPizza.size > tempAppetite)){
+					mediumPizza.quantity++;
+					appetite -= mediumPizza.size;
+				}
+				else if((appetite - (mediumPizza.size + smallPizza.size)) <= 0 && (appetite - (mediumPizza.size + smallPizza.size) > tempAppetite)){
+					smallPizza.quantity++;
+					mediumPizza.quantity++;
+					appetite -= mediumPizza.size + smallPizza.size;
+				}else{
+					largePizza.quantity++;
+					appetite = tempAppetite;
+				}
+			}
+			else if(mediumPizza.isAvailable == true){
+				tempAppetite = appetite - largePizza.size;
+			
+				if((appetite - mediumPizza.size) <= 0 && (appetite - mediumPizza.size > tempAppetite)){
+					mediumPizza.quantity++;
+					appetite -= mediumPizza.size;
+				}
+				else{
+					largePizza.quantity++;
+					appetite = tempAppetite;
+				}
+			}
+			else if(smallPizza.isAvailable == true){
+				tempAppetite = appetite - largePizza.size;
+
+				if((appetite - smallPizza.size) <= 0 && (appetite - smallPizza.size > tempAppetite)){
+					smallPizza.quantity++;
+					appetite -= smallPizza.size;
+				}
+				else{
+					largePizza.quantity++;
+					appetite = tempAppetite;
+				}
+			}
+			else{
+				largePizza.quantity++;
+				appetite -= largePizza.size;
+			}
+		}
+	}
+	else if(mediumPizza.isAvailable == true){
+		
+		mediumPizza.quantity = Math.floor(patrons.appetite()/mediumPizza.size);
+		appetite = patrons.appetite() - (mediumPizza.quantity*mediumPizza.size);
+				
+		if(appetite > 0){
+			if(smallPizza.isAvailable == true){
+				tempAppetite = appetite - mediumPizza.size;
+				
+				if((appetite - smallPizza.size) <= 0 && (appetite - smallPizza.size > tempAppetite)){
+					smallPizza.quantity++;
+					appetite -= smallPizza.size;
+				}
+				else{
+					mediumPizza.quantity++;
+					appetite = tempAppetite;
+				}
+			}
+			else{
+				mediumPizza.quantity++;
+				appetite -= mediumPizza.size;
+			}
+		}
+	}
+	else if(smallPizza.isAvailable == true){
+		
+		smallPizza.quantity = Math.floor(patrons.appetite()/smallPizza.size);
+		appetite = patrons.appetite() - (smallPizza.quantity*smallPizza.size);
+				
+		if(appetite > 0){
+			smallPizza.quantity++;
+			appetite -= smallPizza.size;
+		}
+	}	
+	else{
+		//do nothing
+	}
+			
+	patrons.leftovers = -appetite;
+}
+
+/* function minimizeCost(smallPizza, mediumPizza, largePizza, patrons){
+	
+	var appetite = patrons.appetite();
+	var pizzas = [];
+	if(smallPizza.isAvailable == true){pizzas.push(smallPizza)};
+	if(mediumPizza.isAvailable == true){pizzas.push(mediumPizza)};
+	if(largePizza.isAvailable == true){pizzas.push(largePizza)};
+	
+	sortPizzaCosts(pizzas); //sorts pizza cost with [0] = smallest
+	
+	var tempPizzas = pizzas[];
+	
+	//find boundary conditions
+	var maximum = [];
+	for(i = 0 ; i < pizzas.length() ; i++){
+		maximum[i] = Math.floor(appetite/pizzas[i].size) + 1;
+	} 
+	
+	var sumTotal = 0;
+	for(i = 0 ; i < pizzas.length() ; i++){
+		if(i == 0){
+			sumTotal = maximum[i]*pizzas[i].cost;
+			tempPizza[i].quantity = maximum[i];
+		}
+		else{
+			if(maximum[i]*pizzas[i].cost < sumTotal){
+				sumTotal = maximum[i]*pizzas[i].cost;
+				tempPizzas[] = pizzas[];
+				tempPizza[i].quantity = maximum[i];
+			}
+		}
+	}
+	
+	pizzas = tempPizzas[];
+	
+	//find minimum cost
+	for(i = 0; i < pizzas.length() ; i++){
+		
+	}		
+} */
+
+function sortPizzaCosts(pizzas){
+	pizzas.sort(function(a, b){return a.cost - b.cost});
+}
+
+function calculateSumTotal(pizzas){
+	
+	var sumTotal = 0;
+	
+	for(i = 0; i < pizzas.length() ; i++){
+		sumTotal = sumTotal + (pizzas[i].quantity * pizzas[i].cost);
+	}
+	
+	return sumTotal;
+}
+
+function totalPeople(){
+	var lite = parseInt(document.mainForm.numberOfLiteEaters.value);
+	var regular = parseInt(document.mainForm.numberOfRegularEaters.value);
+	var heavy = parseInt(document.mainForm.numberOfHeavyEaters.value);
+	var total = lite + regular + heavy;
+	
+	if(lite < 0 || regular < 0 || heavy < 0 ){
+		document.getElementById("totalNumberOfPeople").innerHTML = "Invalid entry, must enter a non-negative number";
+	} else{
+		document.getElementById("totalNumberOfPeople").innerHTML = total;
+	}
+	return total;
+}
